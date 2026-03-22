da4c0141d4d3ad3195b8336e6c480b49c7bd67f5
feffy380 <114889020+feffy380@users.noreply.github.com>
2026-03-22 22:36:38 +0100

    wip execution loop and opcodes

---
+++ lib/op.dart
@@ -0,0 +1,3 @@
+enum Op {
+  nop,
+}
---
+++ lib/organism.dart
@@ -0,0 +1,30 @@
+import 'package:lebig/op.dart';
+
+/// Memory and processor state of a digital organism
+class Organism {
+  final int color;
+  // instruction memory: hold instructions
+  late List<Op> program = [];
+  // instruction pointer
+  int ip = 0;
+  // memory. registers or a stack or both
+  // child buffer?
+
+  Organism({required this.color});
+
+  void execute() {
+    var opcode = program[ip];
+
+    switch (opcode) {
+      case Op.nop:
+        break;
+      // TODO: movement instruction
+      // - needs access to world and own ID. could pass world and store own ID,
+      // or pass an execution context object?
+      // - only moves forward, which means we need rotate ops
+      // world.requestMove(id)
+    }
+
+    ip++;
+  }
+}
